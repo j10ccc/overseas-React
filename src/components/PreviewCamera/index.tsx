507209b337f38f4728524aed4f8016f1fae646c2
j10c <blyb1739@gmail.com>
2023-02-18 00:08:09 +0800

    chore: remove .DS_Store

--- src/components/PreviewCamera/index.tsx
+++ src/components/PreviewCamera/index.tsx
@@ -1,43 +1,50 @@
 // import { Face } from "kalidokit";
 import { FaceMesh, FACEMESH_TESSELATION } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, memo } from "react";
 import { drawLandmarks, drawConnectors } from "@mediapipe/drawing_utils";
 import "./index.less";
 
+const CameraInstance = memo(
+  (props: { inputVideoRef: React.MutableRefObject<HTMLVideoElement> }) => {
+    const { inputVideoRef } = props;
+    let camera: Camera;
+    useEffect(() => {
+      if (!inputVideoRef.current) return;
+      camera = new Camera(inputVideoRef.current, {
+        onFrame: async () => {
+          await facemesh.send({ image: inputVideoRef.current as any });
+        },
+        width: 320,
+        height: 240,
+      });
+
+      // 打开摄像头需要 SSL
+      // 开发环境下 localhost
+      camera
+        .start()
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }, []);
+    return (
+      <video
+        className="input-video"
+        ref={inputVideoRef}
+        width="320px"
+        height="240px"
+      ></video>
+    );
+  }
+);
+
 const PreviewCamera = () => {
-  console.log(Camera);
-  const inputVideoRef = useRef<any>();
+  const inputVideoRef = useRef<HTMLVideoElement>();
   const outputCanvasRef = useRef<any>();
 
-  const [inputVideo, setInputVideo] = useState(inputVideoRef.current);
-  const [outputCanvas, setOutputCanvas] = useState(outputCanvasRef.current);
-  const [cameraOn, setCameraOn] = useState(false);
-
-  useEffect(() => {
-    setInputVideo(inputVideoRef.current);
-    console.log("*** video element changed! ***");
-  }, [inputVideoRef]);
-
-  useEffect(() => {
-    setOutputCanvas(outputCanvasRef.current);
-    console.log("*** canvas element changed! ***");
-  }, [outputCanvasRef]);
-
-  const camera = new Camera(inputVideo, {
-    onFrame: async () => {
-      await facemesh.send({ image: inputVideo });
-    },
-    width: 320,
-    height: 240,
-  });
-
-  const handleSwicherClick = () => {
-    if (cameraOn) camera.stop();
-    else camera.start();
-    console.log(cameraOn);
-    setCameraOn(!cameraOn);
-  };
+  // const [cameraOn, setCameraOn] = useState(false);
+  const cameraOn = useRef(false);
 
   const facemesh = new FaceMesh({
     locateFile: (path: string) =>
@@ -58,13 +65,18 @@ const PreviewCamera = () => {
   facemesh.onResults(onResults);
 
   const drawResults = (points: any) => {
-    if (!outputCanvas || !inputVideo || !points) return;
+    if (!outputCanvasRef.current || !inputVideoRef.current || !points) return;
     console.log();
-    outputCanvas.width = inputVideo.videoWidth;
-    outputCanvas.height = inputVideo.videoHeight;
-    let canvasCtx = outputCanvas.getContext("2d");
+    outputCanvasRef.current.width = inputVideoRef.current.videoWidth;
+    outputCanvasRef.current.height = inputVideoRef.current.videoHeight;
+    let canvasCtx = outputCanvasRef.current.getContext("2d");
     canvasCtx.save();
-    canvasCtx.clearRect(0, 0, outputCanvas.width, outputCanvas.height);
+    canvasCtx.clearRect(
+      0,
+      0,
+      outputCanvasRef.current.width,
+      outputCanvasRef.current.height
+    );
     // Use `Mediapipe` drawing functions
     drawConnectors(canvasCtx, points, FACEMESH_TESSELATION, {
       color: "#C0C0C070",
@@ -81,15 +93,10 @@ const PreviewCamera = () => {
 
   return (
     <section className="preview-camera">
-      <video
-        className="input-video"
-        ref={inputVideoRef}
-        width="320px"
-        height="240px"
-      ></video>
+      <CameraInstance inputVideoRef={inputVideoRef} />
       <canvas className="output-canvas" ref={outputCanvasRef}></canvas>
       <div>
-        <button onClick={handleSwicherClick}>
+        <button onClick={() => (cameraOn.current = !cameraOn)}>
           {!cameraOn ? "Start" : "Stop"} Camera
         </button>
       </div>
